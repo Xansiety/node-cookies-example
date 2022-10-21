4b122e90d59834b5dff9f6e4674b8788bd63ba9b
xansiety <ferando543@outlook.com>
2022-10-20 19:15:27 -0500

    Creacion y parametros de configuración de una cookie en respuesta

--- server.js
+++ server.js
@@ -29,6 +29,34 @@ app.get('/', (req, res) => {
     })
 })
 
+
+app.get('/set-cookie', (req, res) => {
+
+
+    //! Puede ser extraida desde el navegador
+    res.cookie("My-Cookie-Name", "Xansiety")
+
+
+    res.cookie("My-Secure-Cookie", "Xansiety Secure",{
+        maxAge: 60000, //Tiempo en milisegundos en que vence esta cookie -> desparece por si sola transcurrido el valor
+        // expires: new Date("2022-10-21") ,// Establece un tiempo de expiracion en formato de fecha JS
+        httpOnly: true, //Para proteger quien puede acceder a esta cookie desde el navegador
+        secure: true, // Solo podemos leer esta cookie si se observa desde un HTTPS es importante para prod
+        sameSite: 'lax' // Es necesaria para poder manipular los dominios si tenemos en backend en cualquier otro dominio
+        // Mismo dominio :  sameSite: 'strict'
+        // Comunicandonos con otro dominio: sameSite: 'lax'
+    })
+
+
+    res.status(200).json({
+        ok: true,
+        msg: "Revisa tu navegador en devtools -> aplicación ->  Cookies"
+    })
+})
+
+
+
+// Escuchar el servidor
 app.listen(port, () => {
     console.log(`Escuchando en http://localhost:${port}` );
 })
